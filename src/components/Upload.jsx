52c7ed8c73a25874d34a64d9f46859a2641f9d2f
김도현 <swputer@gmail.com>
2024-03-10 01:52:07 +0900

    feat: Upload 컴포넌트 수정

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,37 +1,31 @@
-import { useRef } from 'react';
 import { useRecoilState } from 'recoil';
 
 import { dataState } from '@/stores/dataState';
 
 function Upload() {
-  const ref = useRef(null);
   const [data, setData] = useRecoilState(dataState);
 
   const handler = (e) => {
     const files = e.target.files;
     const fileReader = new FileReader();
 
-    fileReader.onload = () =>
-      setData((prev) => ({
-        ...prev,
-        template: fileReader.result,
-      }));
+    fileReader.onload = () => setData((prev) => ({ ...prev, template: fileReader.result }));
     fileReader.readAsText(files[0]);
   };
 
   return (
-    <div className="h-96 w-full">
+    <div className="h-96 w-full select-none">
       {data.template ? (
-        <div className="flex size-full select-none items-center justify-center rounded border-2 border-dashed border-blue-500 text-blue-500">
+        <div className="flex size-full items-center justify-center border-2 border-blue-500 text-blue-500">
           Complete!
         </div>
       ) : (
         <label
           htmlFor="upload"
-          className="flex size-full cursor-pointer items-center justify-center rounded border-2 border-dashed border-gray-200 text-gray-500"
+          className="flex size-full cursor-pointer items-center justify-center border-2"
         >
           Upload File
-          <input id="upload" type="file" ref={ref} className="hidden" onChange={handler} />
+          <input id="upload" type="file" className="hidden" onChange={handler} />
         </label>
       )}
     </div>
