45837942a494fdcd20152fb59748cd0d0b6c4170
김도현 <swputer@gmail.com>
2024-03-10 01:17:37 +0900

    feat: Download 컴포넌트 수정

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,63 +1,49 @@
+import { createElement, useMemo } from 'react';
 import { useRecoilValue } from 'recoil';
-
-import { dataState } from '@/stores/dataState';
 
 import { findAndReplace } from 'hast-util-find-and-replace';
 import { fromHtml } from 'hast-util-from-html';
 import { toHtml } from 'hast-util-to-html';
-import { createElement, useState } from 'react';
+
+import { dataState } from '@/stores/dataState';
+
+const createFile = (tree, data) => {
+  const filename = data[0] + '.html';
+  const pairs = data.slice(1).map((el, index) => [new RegExp(index), el]);
+  const copiedTree = JSON.parse(JSON.stringify(tree));
+
+  findAndReplace(copiedTree, pairs);
+
+  const file = new Blob([toHtml(copiedTree)], { type: 'text/html' });
+
+  return [filename, file];
+};
 
 function Download() {
-  const [a, b] = useState([]);
-  const [isComplete, setComplete] = useState(false);
   const data = useRecoilValue(dataState);
 
-  const createFile = async (ast, data) => {
-    const filename = data[0];
-    const regexp = data.slice(1).map((el, index) => [new RegExp(index), el]);
-    const obj = JSON.parse(JSON.stringify(ast));
+  const downloadLinks = useMemo(() => {
+    const tree = fromHtml(data.template);
+    const links = data.matrix.map((el) => {
+      const [filename, file] = createFile(tree, el);
 
-    findAndReplace(obj, regexp);
-
-    const file = new Blob([toHtml(obj)], { type: 'text/html' });
-
-    b([
-      ...a,
-      createElement(
+      return createElement(
         'a',
         {
+          key: filename,
           href: URL.createObjectURL(file),
-          download: `${filename}.html`,
+          download: filename,
+          className: 'block p-4 hover:bg-gray-100',
         },
-        `${filename}.html`
-      ),
-    ]);
-  };
+        filename
+      );
+    });
 
-  const handleButtonClick = async () => {
-    const templateHtml = fromHtml(data.template);
-
-    await Promise.all(data.matrix.map((m) => createFile(templateHtml, m)));
-
-    setComplete(true);
-  };
+    return links;
+  }, [data]);
 
   return (
-    <div className="h-96 w-full">
-      {isComplete ? (
-        <div className="flex size-full select-none items-center justify-center rounded border-2 border-dashed border-blue-500 text-blue-500">
-          Download Complete!
-          {a.map((el) => el)}
-        </div>
-      ) : (
-        <button
-          onClick={handleButtonClick}
-          className="size-full rounded border-2 border-dashed border-gray-200 text-gray-500"
-        >
-          Create Files
-        </button>
-      )}
-    </div>
+    <div className="h-96 w-full overflow-auto border-2">{downloadLinks.map((link) => link)}</div>
   );
 }
 
